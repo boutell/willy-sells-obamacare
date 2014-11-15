bdf5cc9fb318fce3181c9ea1d1df879165b0d609
Tom Boutell <tom@punkave.com>
2014-11-15 09:49:20 -0500

    it works

---
+++ js/performance-now.js
@@ -0,0 +1,18 @@
+(function(w){
+    var perfNow;
+    var perfNowNames = ['now', 'webkitNow', 'msNow', 'mozNow'];
+    if(!!w['performance']) for(var i = 0; i < perfNowNames.length; ++i)
+    {
+        var n = perfNowNames[i];
+        if(!!w['performance'][n])
+        {
+            perfNow = function(){return w['performance'][n]()};
+            break;
+        }
+    }
+    if(!perfNow)
+    {
+        perfNow = Date.now;
+    }
+    w.perfNow = perfNow;
+})(window);
